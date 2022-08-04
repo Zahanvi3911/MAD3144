225b849349c99c6e2f19e307991ddb5df9215b72
Zahanvi3911 <110626944+Zahanvi3911@users.noreply.github.com>
2022-08-04 13:38:01 -0400

    Add files via upload

---
+++ Second_1.js
@@ -0,0 +1,12 @@
+function addNumber(num1, num2){
+    let i = num1;
+
+    let vari = 0;
+    do{
+        vari = vari + i;
+        i++;
+
+    }while(i<=num2)
+    console.log(vari);
+}
+addNumber(1,10)
